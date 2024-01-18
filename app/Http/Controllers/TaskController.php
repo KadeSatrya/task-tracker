ce9ffd07142fdafc7eff79c2bbebe684ca231c1a
Kadesatrya <kadesatrya@yahoo.co.id>
2024-01-18 21:14:59 +0700

    Added relationship between task and user

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Task;
-use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -17,6 +15,7 @@ class TaskController extends Controller
     public function submit() {
         $task = Task::create(
             [
+                'user_id' => Auth::id(),
                 'title' => request()->input('title'),
                 'description' => request()->input('description')
             ]
@@ -33,8 +32,6 @@ class TaskController extends Controller
     }
 
     public function delete($id){
-        Log::info('Route taken: ' . request()->path());
-
         $task = Task::where('id', $id)->first();
         $task->delete();
         return redirect()->route('home.index');
